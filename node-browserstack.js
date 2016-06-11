a019a69691272f3cfc837352bbbc2d6f5b78fa7b
Jerome D Nelson <jerome.n@icloud.com>
2016-06-11 11:39:48 +0100

    BRANCH FOR TESTING ONLY.

---
+++ node-browserstack.js
@@ -0,0 +1,101 @@
+﻿Long Term Goals for this year
+
+- Driver License and Car
+	- Apply for provisional (requires studio)
+		- May 2016 (£34)
+	
+- Get on Electoral Roll ( April 2016)
+- Get basic credit card (September 2016)
+	- Requires above step	
+
+ - DSA Theory Book (£5 - £10) (April 2016)
+ - Practice Lessons (Research price)
+	(Both of the below depend on training)
+	- Theory Test (£23)
+	- Practical Test (£64)
+	
+- Solid Spanish Language sills
+	 - El Alfabeto e Los Nombres (cero to 1,000,000)
+		- LL e Y
+		- B e V
+		- Ch
+		- ñ
+	- How to constrcut sentences
+		- About myself
+		- About others
+		- Getting Instructions
+		- Telling TIme
+		- 30 minutes of fluencia every day/ 30 minutes of Las Noticias todo el dias.
+		- TRAVEL!! Go to Spanish Cinema!
+		- Find/Start intercambio (Spanish talking exchange)
+		- Teclado <Country> i.e Teclado Español		  
+		  - Sign up for a class
+			* RESEARCH LOCAL CLASSES AND PRICES
+		  - Get a Spanish Dictionary
+			* WAIT TILL PAYDAY
+		
+ LAST - Speak only in Spanish for one month
+
+- See Physiotherapist regarding alignment and ankles 
+- 10% Body Fat (April 2017)
+	- Focus on Circiut Training to improve martial fitness (Flexibility, Explosive speed and long term endurance/stamina)
+		* Power/Explosive Strength
+		* Stamina 
+			- Instead of just vanilla running, try extra activities whilst doing so.
+		* Fat Loss (Integrate into exercises)
+//		* HIIT
+		* Warm Up/Cool Down
+		* Power Circuit Training
+			- Idea is centered around regular training
+				* Inbetween regular circuits pick supplementary exercises
+					- one arm clean, one arm jerk
+		 - Key Point
+			- Intake less calories to reduce body weight
+			- Take in more to build more for muscle mass.
+			* Find out 
+		- Emphasis on fat loss
+	- Diet (Use exercise tracker, with calorie tracker as well)
+	
+	* Warm Up Routine
+		* Mohm Puhl Ki
+	* Running Exercise
+
+	=== THIS SECTION NEEDS RESEARCH AND AMENDING
+[	* Horse Stance Stretching ||
+	* Circuit Training ||
+	* Distance Running ||
+]
+	* Cool Down Stretches
+	
+- Learn to Swim (Start September 2016), Improve Table Tennis skills (May 2016)
+- Commit to BJJ (Aug 2016)
+- Tai Chi or Yoga (May 2016)
+- Horse Stance and able to touch floor with hands (March 2016)
+- Be able to replicate and outperform Mohm Puhl Ki (March 2016)
+
+
+- TTT2 e T7 - estudiare moveset de Jin e improve movement on stick (On-going - doesn't stop)
+	- Create a play plan for this.
+	
+- More travel e meet more ppl.
+
+- start investing (create investment plan and implement this year - have it verified).
+- Start Regular savings (April 2016)
+	- Account for all costs, minimize and then bank whatever is left.
+	- From April onwards £1000 of salary.
+	
+- Improve javascript ()
+	- Look into Jquery library 
+	- Understand javascript terminlogy
+- Create schedule and finish backlog of personal projects (may take longer than one year).
+
+- Maintenance Habits
+	- Feet, Nails, Hair, Face, Teeth (impt!!)
+- Complete GCSE-A'Level Mathematics 
+	- Part-time degree
+- Gain Credit History
+- 
+
+- Halloween Cake Bake
+	- Polenta Orange cake (styled into Pumpkins)
+		- Nestled on top of Cake with White Chocolate Ganache (coloured red) - Opera Cake, Blueberry Lemon Cake
